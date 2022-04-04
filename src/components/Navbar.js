c02b4a5fb857c77b4bd67fc25d932d6c16578b93
Cristian Eduardo Rojas <credrope@hotmail.com>
2022-04-04 13:49:19 -0500

    Cambio de titulo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ const Navbar = (props) => {
   return (
     <nav className='navbar'>
       <div className='navbar-content'>
-        <h3>UseReducer</h3>
+        <h3>Mi Cart</h3>
         <div className='cart'>
           Cart
           <span className='number'>{props.amount()}</span>
